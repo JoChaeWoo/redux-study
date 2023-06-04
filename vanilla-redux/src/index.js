42a8da3d4bc048681a2c7904c3dc471a90d20483
JoChaeWoo <jcy6343@naver.com>
2023-06-04 16:55:41 +0900

    [feat] redux store 추가, todo list 변경 기능 추가

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -45,20 +45,62 @@ function initTodos() {
   const input = document.querySelector("input")
   const ul = document.querySelector("ul#todos")
 
-  const createTodo = (toDo) => {
-    const li = document.createElement("li");
-    li.innerText = toDo
-    ul.appendChild(li);
+  const ADD_TODO = "ADD_TODO"
+  const DELETE_TODO = "DELETE_TODO"
+
+  const addTodo = (toDo) => {
+    return {
+      type: ADD_TODO,
+      text: toDo
+    }
   }
+
+  const deleteTodo = (id) => {
+    return {
+      type: DELETE_TODO,
+      id
+    }
+  }
+
+  const reducer = (state = [], action) => {
+    switch (action.type) {
+      case ADD_TODO:
+        return [{text: action.text, id: Date.now()}, ...state]
+      case DELETE_TODO:
+        return state.filter(todo => todo.id !== action.id)
+      default:
+        return state
+    }
+  }
+  const paintToDos = () => {
+    const toDos = todoStore.getState()
+    console.log(toDos)
+    ul.innerHTML = toDos.map(todo => `<li id="${todo.id}">${todo.text}<button>DEL</button></li>`).join("")
+  }
+
+  const dispatchAddToDo = (text) => {
+    todoStore.dispatch(addTodo(text))
+  }
+
+  const dispatchDeleteToDo = (e) => {
+    if(e.target.tagName !== "BUTTON") return
+    const li = e.target.closest("li")
+    todoStore.dispatch(deleteTodo(parseInt(li.id, 10)))
+  }
+
+  const todoStore = createStore(reducer)
   
+  todoStore.subscribe(paintToDos)
+
   const onSubmit = (e) => {
     e.preventDefault()
     const toDo = input.value
     input.value = ""
-    createTodo(toDo)
+    dispatchAddToDo(toDo)
   }
 
   form.addEventListener("submit", onSubmit)
+  ul.addEventListener("click", dispatchDeleteToDo)
 }
 
 initCounter()
