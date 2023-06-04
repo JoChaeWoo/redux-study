2b49c5796ccdf169df614e7fb76ada9e3b2ecc97
JoChaeWoo <jcy6343@naver.com>
2023-06-04 16:21:46 +0900

    [feat] 기본적인 todo list 컴포넌트 추가

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,39 +1,65 @@
 import { createStore } from "redux"
 
-const plus = document.querySelector("button#plus")
-const minus = document.querySelector("button#minus")
-const number = document.querySelector("span")
+function initCounter() {  
+  const plus = document.querySelector("button#plus")
+  const minus = document.querySelector("button#minus")
+  const number = document.querySelector("span")
 
-number.innerText = 0
+  number.innerText = 0
 
-const ADD = "ADD"
-const MINUS = "MINUS"
+  const ADD = "ADD"
+  const MINUS = "MINUS"
 
-const countModifier = (state = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return state + 1
-    case MINUS:
-      return state - 1
-    default:
-      return state
+  const countModifier = (state = 0, action) => {
+    switch (action.type) {
+      case ADD:
+        return state + 1
+      case MINUS:
+        return state - 1
+      default:
+        return state
+    }
   }
+  const countStore = createStore(countModifier)
+
+  const onChange = () => {
+    number.innerText = countStore.getState()
+  }
+
+  countStore.subscribe(onChange)
+
+  const handlePlus = () => {
+    countStore.dispatch({ type: ADD })
+  }
+  const handleMinus = () => {
+    countStore.dispatch({ type: MINUS })
+  }
+
+  plus.addEventListener("click", handlePlus)
+  minus.addEventListener("click", handleMinus)
+
 }
-const countStore = createStore(countModifier)
 
-const onChange = () => {
-  number.innerText = countStore.getState()
+function initTodos() {
+  const form = document.querySelector("form")
+  const input = document.querySelector("input")
+  const ul = document.querySelector("ul#todos")
+
+  const createTodo = (toDo) => {
+    const li = document.createElement("li");
+    li.innerText = toDo
+    ul.appendChild(li);
+  }
+  
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const toDo = input.value
+    input.value = ""
+    createTodo(toDo)
+  }
+
+  form.addEventListener("submit", onSubmit)
 }
 
-countStore.subscribe(onChange)
-
-const handlePlus = () => {
-  countStore.dispatch({ type: ADD })
-}
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS })
-}
-
-plus.addEventListener("click", handlePlus)
-minus.addEventListener("click", handleMinus)
-
+initCounter()
+initTodos()
