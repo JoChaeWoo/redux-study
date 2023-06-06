fa3a6f5a4e598362cd736f961f1a353b6caf3b4a
JoChaeWoo <jcy6343@naver.com>
2023-06-06 18:39:42 +0900

    [refactor] console.log 제거

--- react-redux/src/routes/Detail.js
+++ react-redux/src/routes/Detail.js
@@ -2,13 +2,11 @@ import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 function Detail(props) {
-  console.log(props)
   const { id } = useParams()
   const todo = useSelector((state) => {
     console.log(state, id)
     return state.find((todo) => todo.id === parseInt(id, 10))
   })
-  console.log(todo)
   return (
     <>
       <h2>{todo?.text}</h2>
