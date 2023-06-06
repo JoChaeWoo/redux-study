e4da5d6522ddc71b238a91efbd16c6195370450d
JoChaeWoo <jcy6343@naver.com>
2023-06-06 17:27:03 +0900

    [feat] detail 페이지 추가

--- react-redux/src/components/Todo.js
+++ react-redux/src/components/Todo.js
@@ -1,17 +1,20 @@
 import { connect } from 'react-redux'
 import { actionCreators } from '../store'
+import { Link } from 'react-router-dom'
 
-function Todo({ text, onBntClick }) {
+function Todo({ text, id, onBntClick }) {
   return (
     <li>
-      {text} <button onClick={onBntClick}>DEL</button>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBntClick}>DEL</button>
     </li>
   )
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
-    onBntClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+    onBntClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+    ...ownProps
   }
 }
 
--- react-redux/src/routes/Detail.js
+++ react-redux/src/routes/Detail.js
@@ -1,3 +1,20 @@
-const Detail = () => "Detail"
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+
+function Detail(props) {
+  console.log(props)
+  const { id } = useParams()
+  const todo = useSelector((state) => {
+    console.log(state, id)
+    return state.find((todo) => todo.id === parseInt(id, 10))
+  })
+  console.log(todo)
+  return (
+    <>
+      <h2>{todo?.text}</h2>
+      <h4>Created at: {todo?.id}</h4>
+    </>
+  )
+}
 
 export default Detail
