9b5f7f2dce7c5bc1d0bce198c4e76f826e01d64e
JoChaeWoo <jcy6343@naver.com>
2023-06-06 15:27:18 +0900

    [feat] 입력창 구현

--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,3 +1,24 @@
-const Home = () => "Home"
+import { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setText("")
+  }
+  
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange} />
+        <button>Add</button>
+      </form>
+    </>
+  )
+}
 
 export default Home
